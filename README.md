0a7b891c98dbf63af55a94881d6fbaa5103b861b
Robin Wyss <hello@robinwyss.com>
2021-12-20 09:17:07 +0100

    added documentation, removed unused script

--- README.md
+++ README.md
@@ -2,12 +2,31 @@
 
 ### [export_vulnerabilities.py](export_vulnerabilities.py)
 exports all vulnerabilites to a CSV file
-
-### [log4j_versions.py](log4j_versions.py)
-Generates a CSV with all versions of log4j and the hosts and processes on which they are used. 
+##### Examples
+```bash
+libraries_by_host.py -e https://xxxyyyyy.live.dynatrace.com -t dt0c01.XXX... 
+```
+Additionaly fetch details for each vulnerability
+```bash
+libraries_by_host.py -e https://xxxyyyyy.live.dynatrace.com -t dt0c01.XXX... -d
+```
 
 ### [softwareComponents4pgi.py](softwareComponents4pgi.py)
 Exports all Software Components for a given Process Group Instance
+##### Examples
+```bash
+libraries_by_host.py -e https://xxxyyyyy.live.dynatrace.com -t dt0c01.XXX... -id PROCESS_GROUP_INSTANCE_XXX
+```
 
 ### [libraries_by_host.py](libraries_by_host.py)
 Exports a list of all hosts with information about processes and libraries
+
+##### Examples
+Retrieve all libraries from all hosts
+```bash
+libraries_by_host.py -e https://xxxyyyyy.live.dynatrace.com -t dt0c01.XXX... 
+```
+Filter by a specific library (e.g. log4j)
+```bash
+libraries_by_host.py -e https://xxxyyyyy.live.dynatrace.com -t dt0c01.XXX... -l org.apache.logging.log4j
+```
--- libraries_by_host.py
+++ libraries_by_host.py
@@ -5,28 +5,50 @@ import functools
 import csv
 
 # get the Dynatrace Environmemnt (URL) and the API Token with arguments
-# with the details parameter, the details for each security problem are fetched
 parser = ArgumentParser()
 parser.add_argument("-e", "--env", dest="environment", help="The Dynatrace Environment to query", required=True)
 parser.add_argument("-t", "--token", dest="token", help="The Dynatrace API Token to use", required=True)
+parser.add_argument("-l", "--library", dest="library", help="Filter resulsts by a specific library", required=False)
 
 args = parser.parse_args()
 
 env = args.environment
 apiToken = args.token
+libraryToFilterBy = args.library
 processType = 'JAVA'
 
 def getSoftwareComponents(softwareComponents):
+    """
+    Retrieves the details of the specfied software components
+    :param list of entity references (dic) (e.g. [{'id': ...}])
+    :return list of entities (dictionary)
+    """
     return getAllEntitiesByIDs('/api/v2/entities?fields=fromRelationships,properties.packageName,properties.softwareComponentFileName,properties.softwareComponentShortName,properties.softwareComponentType', softwareComponents)
 
 def getProcesses(processes):
+    """
+    Retrieves the details of the specfied processes, with thechnolgy details and the relations to software components
+    :param list of entity references (dic) (e.g. [{'id': ...}])
+    :return list of entities (dictionary)
+    """
     return getAllEntitiesByIDs('/api/v2/entities?fields=toRelationships.isSoftwareComponentOfPgi,properties.processType,properties.softwareTechnologies', processes)
 
 def getHosts():
+    """
+    Get all hosts with the relationships to processes (PGIs)
+    :return list of entities (dictionary)
+    """
     return getAllEntities('/api/v2/entities?pageSize=500&fields=+toRelationships.isProcessOf&entitySelector=type("HOST")')
 
 def getAllEntitiesByIDs(endpoint, entityRefs):
+    """
+    Retrieves all entities by the specified entity references.
+    param: string endpoint: the API endpoint to call
+    param: list entityRefs: entities to be retrieved
+    return: list of entities (dictionary) 
+    """
     entities = []
+    # split the list into chunks of 100 in order to avoid too large requests (URI too long)
     listOfEntityIds = splitIntoChunks(entityRefs, 100)
     for entitieIds in listOfEntityIds:
         ids = getIdsFromEntities(entitieIds)
@@ -34,6 +56,11 @@ def getAllEntitiesByIDs(endpoint, entityRefs):
     return entities
 
 def getAllEntities(endpoint):
+    """
+    Retrieves all entities by the specified api call (handles paging of results)
+    param: str endpoint: the API endpoint to call
+    return: list of entities (dictionary) 
+    """
     entities = []
     response = queryApi(endpoint)
     entities += response["entities"]
@@ -43,27 +70,47 @@ def getAllEntities(endpoint):
     return entities
 
 def getIdsFromEntities(entities):
+    """
+    Combines the IDs of the entities into a string to be used in an API call (e.g. '"ID1","ID2..."')
+    param: list of entities
+    return string
+    """
     return ','.join('"'+i['id']+'"' for i in entities)
 
-# splits a list into of max length n
 def splitIntoChunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
 def queryApi(endpoint):
+    """
+    Calls the given endpoint on the Dynatrace API. 
+    param: string endpoint: API endpoint to be called
+    return: response as json
+    """
     authHeader = {'Authorization' : 'Api-Token '+ apiToken}
     response = requests.get(env + endpoint, headers=authHeader)
     print('.', end="", flush=True) # print a dot for every call to show activity
     return response.json()
 
 def getProperty(entity, propertyName):
+    """
+    Retrieves the value of a property from an entity if it exists, otherwise an empty string
+    param: dictionary entity: the entity from which the property should be retrieved
+    param: string propertyName: the property to be retrieved
+    return: string: the value of the property or empty string if it doesn' exist.
+    """
     if propertyName in entity['properties']:
         return entity['properties'][propertyName]
     else:
         return ""
 
 def getTechnologyVersion(process):
+    """
+    Gets the technology information from a process
+    param: dictionary entity: the process entity from which the information should be retrieved
+    return string: technology version
+    """
     softwareTechnologies = getProperty(process, 'softwareTechnologies')
     for technology in softwareTechnologies:
         if technology['type'] == processType and 'version' in technology:
@@ -87,6 +134,8 @@ with open('libraries_by_host.csv', 'w', newline='') as f:
                     if 'isSoftwareComponentOfPgi' in process['toRelationships']:
                         softwareComponentRefs = process['toRelationships']['isSoftwareComponentOfPgi']
                         softwareComponents = getSoftwareComponents(softwareComponentRefs)
+                        if libraryToFilterBy:
+                            softwareComponents = filter(lambda e: e['displayName'].startswith(libraryToFilterBy), softwareComponents)
                         for softwareComponent in softwareComponents:
                             writer.writerow([
                                 host['displayName'],
